c2f2fe9b2f25301144d24398b1945206157c7d48
AGB92 <adam.geborek@gmail.com>
2024-05-21 02:00:11 +0200

    kolejne zadania
    kolejne zadania

---
+++ Warsztat/Library/Library.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Library.Domain;
+using Library.Persistence;
+using System.Diagnostics;
+
+namespace Library.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //zadanie 1
+                Book book = new Book("Paragraf 22","Joseph Heller",1961, "978-83-798-5503-2", 5, 18.99m);
+                BooksRepository repository = new BooksRepository();
+
+            //zadanie 2
+                Console.WriteLine("\nPodaj login:");
+                string login = Console.ReadLine();
+
+                Console.WriteLine("\nPodaj hasło:");
+                string haslo = Console.ReadLine();
+
+                if (login == "Admin" && haslo == "password")
+                {
+                    Console.WriteLine("Access Granted");
+                }
+                else
+                {
+                    Console.WriteLine("Access Denied");
+                }
+
+            //Zadanie 3: Utworzenie menu w aplikacji konsol / Pętle sterujące
+                
+
+        }
+    }
+}
